3912526a88744af9317d6cd72d7b66760bffe6aa
Alberto <albertoasorey@gmail.com>
2023-09-07 20:13:01 +0200

    Version inicial. Clase 4, Hello Word

---
+++ src/main/java/clase4/HelloWord.java
@@ -0,0 +1,15 @@
+package clase4;
+
+import java.util.Scanner;
+
+class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello, World Alb!");
+
+        String nombre = "Alberto";
+        Integer nEquipo = 1;
+        String texto = "Hola soy " + nombre + ", del equipo nro. " + nEquipo;
+        System.out.println(texto);
+    }
+
+}
